100cc890dd8f31f535603877f8d5568e2412787b
NoiBuilds <lekhalontsoaki@gmal.com>
2025-05-16 12:36:46 +0200

    express working on local

--- routes/invoice.routes.js
+++ routes/invoice.routes.js
@@ -18,4 +18,13 @@ router.post('/', upload.single('logo'), invoiceController.createInvoice);
 router.get('/', invoiceController.getInvoices);
 router.post('/send', invoiceController.sendInvoice);
 
+router.delete('/:id', async (req, res) => {
+  try {
+    await Invoice.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete invoice' });
+  }
+});
+
 module.exports = router;
